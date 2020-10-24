d12350658a414a3e2c104f31c1b1f2ce9742641a
z-haiman <zhaiman@eng.ucsd.edu>
2020-10-23 20:45:07 -0700

    filename changes

--- validate/execute_notebooks.sh
+++ validate/execute_notebooks.sh
@@ -5,14 +5,14 @@ set -e
 cd $( cd "$(dirname "$0")" ; pwd -P )
 
 # Execute notebooks in order
-for notebook in 'Glycolysis' \
-                'Hemoglobin' \
-                'HEX1' \
-                'PFK' \
-                'PYK' \
-                'Non-personalized_Model_construction' \
-                'Non-personalized_Model_validation' \
-                'Fig2_panels' ; do \
+for notebook in 'glycolysis' \
+                'hemoglobin' \
+                'hex1' \
+                'pfk' \
+                'pyk' \
+                'bon-personalized_model_construction' \
+                'non-personalized_model_validation' \
+                'fig2_panels' ; do \
     echo "Executing $notebook.ipynb"
     jupyter nbconvert --execute "$notebook.ipynb" --to notebook \
         --ExecutePreprocessor.timeout=-1 \
